e70e23a5909588d4bec7ae1b6203c9860139ecd0
Shuheng Liu <wish1104@icloud.com>
2021-02-06 16:59:58 +0800

    test(condition): add test case for spherical shift dirichlet condition

--- tests/test_conditions.py
+++ tests/test_conditions.py
@@ -4,7 +4,7 @@ from neurodiffeq.networks import FCNN
 from neurodiffeq.generators import Generator1D, SamplerGenerator
 from neurodiffeq.function_basis import ZonalSphericalHarmonics
 
-from conditions import ReverseBVP, SphericalDirichletBVP
+from conditions import ReverseBVP, SphericalDirichletBVP, SphericalShiftDirichletBVP
 
 N_SAMPLES = 100
 degrees = [0, 2, 4, 6, 8]
@@ -27,6 +27,19 @@ def test_spherical_dirichlet_bvp():
     R0 = torch.rand(len(degrees))
     R1 = torch.rand(len(degrees))
     condition = SphericalDirichletBVP(r0, R0, r1, R1, normalizer=np.random.rand())
+    r = r0 * torch.ones(N_SAMPLES, 1)
+    assert torch.isclose(condition.enforce(net, r), R0).all()
+    r = r1 * torch.ones(N_SAMPLES, 1)
+    assert torch.isclose(condition.enforce(net, r), R1).all()
+
+
+def test_spherical_shift_dirichlet_bvp():
+    net = FCNN(1, 1)
+    r0 = np.random.rand()
+    r1 = np.random.rand() + r0
+    R0 = torch.rand(len(degrees))
+    R1 = torch.rand(len(degrees))
+    condition = SphericalShiftDirichletBVP(r0, R0, r1, R1)
     r = r0 * torch.ones(N_SAMPLES, 1)
     assert torch.isclose(condition.enforce(net, r), R0).all()
     r = r1 * torch.ones(N_SAMPLES, 1)
