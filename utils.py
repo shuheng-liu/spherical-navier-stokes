56a7eb77bc9dd830abfb526d5c9c2fc5feaef596
Shuheng Liu <wish1104@icloud.com>
2021-02-06 19:00:01 +0800

    feat(utils): add utils for `mkdir -p` and `touch`

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import functools
+from pathlib import Path
 
 
 def partial_class(cls, *args, **kwargs):
@@ -6,3 +7,11 @@ def partial_class(cls, *args, **kwargs):
         __init__ = functools.partialmethod(cls.__init__, *args, **kwargs)
 
     return NewClass
+
+
+def safe_makedir(dir):
+    Path(dir).mkdir(parents=True, exist_ok=True)
+
+
+def safe_touch(fname, mode=664):
+    Path(fname).touch(mode=664, exist_ok=True)
