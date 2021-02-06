49470e98375b650baf2973bd3f552bd79ec5ee6e
Shuheng Liu <wish1104@icloud.com>
2021-02-07 00:51:49 +0800

    feat(session): impl. `Session.set_equations`

--- session.py
+++ session.py
@@ -1,4 +1,6 @@
 from weighting import ScalarComposition, get_fn_by_name, WeightedResiduals
+from equations import ZonalHarmonicsNS
+from neurodiffeq.function_basis import ZonalSphericalHarmonics
 from config import Config
 
 
@@ -14,3 +16,15 @@ class Session:
             )
             for eq, cfg in weighting_cfg.items()
         }
+
+    def set_equations(self, pde_cfg, numerical_cfg):
+        harmonics_fn = ZonalSphericalHarmonics(degrees=numerical_cfg.degrees)
+        self.pdes = ZonalHarmonicsNS(
+            rho=pde_cfg.rho,
+            mu=pde_cfg.mu,
+            omega0=pde_cfg.omega0,
+            omega1=pde_cfg.omega1,
+            r0=pde_cfg.r0,
+            r1=pde_cfg.r1,
+            harmonics_fn=harmonics_fn,
+        )
