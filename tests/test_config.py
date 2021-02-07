21eb5d980f947e02a21beb04d6a0cd27d15082d7
Shuheng Liu <wish1104@icloud.com>
2021-02-07 21:19:14 +0800

    test(config): add test case for array config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,64 @@
+import pytest
 from config import Config
 
 
-def test_config():
-    d1 = dict(
+@pytest.fixture
+def d1():
+    return dict(
         a=dict(a=1, b=2),
         b=3,
     )
-    config = Config(d1)
+
+
+@pytest.fixture
+def d2():
+    return dict(
+        a=dict(b=4, c=5),
+        c=dict(e=6, f=7),
+    )
+
+
+@pytest.fixture
+def d3():
+    return {
+        'a': 1,
+        'b': [
+            {'c': 2},
+            [
+                {'d': 3},
+                {'e': {}},
+            ]
+        ]
+    }
+
+
+def test_config_list(d3):
+    config = Config.auto_convert(d3)
+    assert isinstance(config, Config)
     assert config._name == 'config'
+    assert config.a == 1
+    assert isinstance(config.b, Config)
+    assert config.b._name == 'config.b'
+    assert isinstance(config.b[0], Config)
+    assert config.b[0]._name == 'config.b.0'
+    assert config.b[0].c == 2
+    assert isinstance(config.b[1], Config)
+    assert config.b[1]._name == 'config.b.1'
+    assert isinstance(config.b[1][0], Config)
+    assert config.b[1][0]._name == 'config.b.1.0'
+    assert config.b[1][0].d == 3
+    assert isinstance(config.b[1][1], Config)
+    assert config.b[1][1]._name == 'config.b.1.1'
+    assert isinstance(config.b[1][1].e, Config)
+
+
+def test_config(d1, d2):
+    config = Config.auto_convert(d1)
+    assert config._name == 'config'
+    assert config.is_dict and not config.is_list
     assert isinstance(config.a, Config)
-    assert config.a._name == 'a'
+    assert config.a._name == 'config.a'
+    assert config.a.is_dict and not config.a.is_list
     assert config.a.a == 1
     assert config.a.b == 2
     assert config.b == 3
@@ -21,19 +70,18 @@ def test_config():
         else:
             assert isinstance(v, int)
 
-    d2 = dict(
-        a=dict(b=4, c=5),
-        c=dict(e=6, f=7),
-    )
     config.update(d2)
     assert config._name == 'config'
+    assert config.is_dict and not config.is_list
     assert isinstance(config.a, Config)
-    assert config.a._name == 'a'
+    assert config.a._name == 'config.a'
+    assert config.a.is_dict and not config.a.is_list
     assert config.a.a == 1
     assert config.a.b == 4
     assert config.a.c == 5
     assert config.b == 3
-    assert config.c._name == 'c'
+    assert config.c._name == 'config.c'
+    assert config.c.is_dict and not config.c.is_list
     assert isinstance(config.c, Config)
     assert config.c.e == 6
     assert config.c.f == 7
