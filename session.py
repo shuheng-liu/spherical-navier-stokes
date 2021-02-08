b0c5f8698f87a80ef4819bd72207e60a9669b4e8
Shuheng Liu <wish1104@icloud.com>
2021-02-09 00:34:51 +0800

    refactor(session): flatten logic of Session.__init__()

--- session.py
+++ session.py
@@ -12,18 +12,10 @@ from config import Config
 
 class Session:
     def __init__(self, cfg):
-        self.set_weighting(cfg)
-        self.set_equations(cfg)
-        self.set_networks(cfg)
-        self.set_optimizer(cfg)
-        self.set_monitors(cfg)
-        self.set_curriculum(cfg)
-
-    def set_weighting(self, cfg):
         weighting_cfg = cfg.weighting
         if weighting_cfg is None:
             raise ValueError('weight_cfg = None')
-
+        # set weighting
         self.weight_fns = {
             eq: ScalarComposition(
                 fn=get_fn_by_name(cfg.type)(**cfg.__dict__['args']),
@@ -31,29 +23,24 @@ class Session:
             )
             for eq, cfg in weighting_cfg.items()
         }
-
-    def set_equations(self, cfg):
-        pde_cfg = cfg.pde
-        self.harmonics_fn = ZonalSphericalHarmonics(degrees=list(pde_cfg.degrees.items()))
+        # set function basis
+        self.harmonics_fn = ZonalSphericalHarmonics(degrees=list(cfg.pde.degrees.items()))
+        # set equations
         self.pdes = ZonalHarmonicsNS(
-            rho=pde_cfg.rho,
-            mu=pde_cfg.mu,
-            omega0=pde_cfg.omega0,
-            omega1=pde_cfg.omega1,
-            r0=pde_cfg.r0,
-            r1=pde_cfg.r1,
+            rho=cfg.pde.rho,
+            mu=cfg.pde.mu,
+            omega0=cfg.pde.omega0,
+            omega1=cfg.pde.omega1,
+            r0=cfg.pde.r0,
+            r1=cfg.pde.r1,
             harmonics_fn=self.harmonics_fn,
         )
-
-    def set_networks(self, cfg):
+        # set networks
         self.nets = [ModelFactory.from_config(c) for _, c in cfg.network.items()]
-
-    def set_optimizer(self, cfg):
+        # set optimizer
         optimizer_getter = OptimizerFactory.from_config(cfg.optimizer)
         self.optimizer = optimizer_getter(chain.from_iterable(n.parameters() for n in self.nets))
-
-    def set_monitors(self, cfg):
+        # set monitors
         self.monitor_callbacks = MonitorCallbackFactory.from_config(cfg.monitor, self.pdes, self.harmonics_fn)
-
-    def set_curriculum(self, cfg):
+        # set curriculum
         self.curriculum = CurriculumFactory.from_config(cfg)
