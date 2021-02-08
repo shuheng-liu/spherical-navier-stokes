e50ce59e4115bbc1160516d4608ca18180b868bc
Shuheng Liu <wish1104@icloud.com>
2021-02-08 23:58:19 +0800

    feat(session): impl. Session.set_monitors

--- session.py
+++ session.py
@@ -2,12 +2,22 @@ from itertools import chain
 from weighting import ScalarComposition, get_fn_by_name, WeightedResiduals
 from equations import ZonalHarmonicsNS
 from neurodiffeq.function_basis import ZonalSphericalHarmonics
+from neurodiffeq.monitors import MonitorSphericalHarmonics
 from networks import ModelFactory
 from optimizers import OptimizerFactory
+from monitors import MonitorCallbackFactory
 from config import Config
 
 
 class Session:
+    def __init__(self, cfg):
+        self.set_weighting(cfg)
+        self.set_equations(cfg)
+        self.set_networks(cfg)
+        self.set_optimizer(cfg)
+        self.set_monitors(cfg)
+        self.set_curriculum(cfg)
+
     def set_weighting(self, cfg):
         weighting_cfg = cfg.weighting
         if weighting_cfg is None:
@@ -23,6 +33,7 @@ class Session:
 
     def set_equations(self, cfg):
         pde_cfg = cfg.pde
+        self.harmonics_fn = ZonalSphericalHarmonics(degrees=list(pde_cfg.degrees.items()))
         self.pdes = ZonalHarmonicsNS(
             rho=pde_cfg.rho,
             mu=pde_cfg.mu,
@@ -30,7 +41,7 @@ class Session:
             omega1=pde_cfg.omega1,
             r0=pde_cfg.r0,
             r1=pde_cfg.r1,
-            harmonics_fn=ZonalSphericalHarmonics(degrees=list(pde_cfg.degrees.items())),
+            harmonics_fn=self.harmonics_fn,
         )
 
     def set_networks(self, cfg):
@@ -39,3 +50,10 @@ class Session:
     def set_optimizer(self, cfg):
         optimizer_getter = OptimizerFactory.from_config(cfg.optimizer)
         self.optimizer = optimizer_getter(chain.from_iterable(n.parameters() for n in self.nets))
+
+    def set_monitors(self, cfg):
+        self.monitor_callbacks = MonitorCallbackFactory.from_config(cfg.monitor, self.pdes, self.harmonics_fn)
+
+    def set_curriculum(self, cfg):
+        # TODO set curriculum
+        pass
