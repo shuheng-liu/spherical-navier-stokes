c1b91aab33f4826b0dd6cd0d6610d817569b10b2
Shuheng Liu <wish1104@icloud.com>
2021-02-09 16:20:15 +0800

    test(session): simplify test case that involves training

--- tests/test_session.py
+++ tests/test_session.py
@@ -9,6 +9,7 @@ from session import Session
 from weighting import ScalarComposition, get_fn_by_name, SoftStep
 from config import Config
 from curriculum import BaseCurriculumLearner
+from copy import deepcopy
 
 
 @pytest.fixture
@@ -19,6 +20,44 @@ def root_config():
 @pytest.fixture
 def s(root_config):
     return Session(root_config)
+
+
+@pytest.fixture
+def modified_config(root_config):
+    small_net = dict(
+        module_type='fcnn',
+        args=[1, 1],
+        kwargs=dict(
+            hidden_units=(32,),
+            actv='swish'
+        )
+    )
+    config = deepcopy(root_config)
+    config.update(dict(
+        pde=dict(
+            degrees=[0]
+        ),
+        network=dict(
+            ur=small_net,
+            utheta=small_net,
+            uphi=small_net,
+            p=small_net,
+        ),
+        curriculum=dict(
+            base_size=32,
+            min_size=1,
+            max_size=32,
+            epochs_per_curriculum=1,
+            n_curricula=2,
+        ),
+    ))
+    config.__dict__['monitor'] = {}
+    return config
+
+
+@pytest.fixture
+def s2(modified_config):
+    return Session(modified_config)
 
 
 def test_weighting(root_config, s):
@@ -65,11 +104,18 @@ def test_conditions(root_config, s):
         assert isinstance(c, BaseCondition)
 
 
-def test_solver(root_config, s):
-    assert isinstance(s.solver, BaseSolver)
-    weights_before = [p.detach().cpu().numpy() for n in s.nets for p in n.parameters()]
+def test_solver(modified_config, s2):
+    assert isinstance(s2.solver, BaseSolver)
+    weights_before = [p.detach().cpu().numpy() for n in s2.nets for p in n.parameters()]
     w_before = np.concatenate([w.flatten() for w in weights_before])
-    s.solver.fit(max_epochs=1)
-    weights_after = [p.detach().cpu().numpy() for n in s.nets for p in n.parameters()]
+    s2.solver.fit(max_epochs=1)
+    weights_after = [p.detach().cpu().numpy() for n in s2.nets for p in n.parameters()]
     w_after = np.concatenate([w.flatten() for w in weights_after])
     assert (w_before != w_after).any()
+
+
+def test_fit(modified_config, s2):
+    s2.fit()
+    assert s2.solver.global_epoch \
+           == modified_config.curriculum.n_curricula \
+           * modified_config.curriculum.epochs_per_curriculum
