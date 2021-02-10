1e6726de97f0fc4a107620b0765cda5a5d387a21
Shuheng Liu <wish1104@icloud.com>
2021-02-10 17:15:42 +0800

    chore(pypi): add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="spherical-navier-stokes",  # Replace with your username
+    version="0.1.0",
+    author="Shuheng Liu",
+    author_email="wish1104@icloud.com",
+    description="Spherical Navier Stokes",
+    url="https://github.com/shuheng-liu/spherical-navier-stokes",
+    packages=setuptools.find_packages(),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
