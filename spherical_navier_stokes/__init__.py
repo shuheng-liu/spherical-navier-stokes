fdb06d0c7afd47f82a2c794986a278ba2d7e7f59
Shuheng Liu <wish1104@icloud.com>
2021-02-10 21:27:51 +0800

    feat(logging): set default logging level to info

--- spherical_navier_stokes/__init__.py
+++ spherical_navier_stokes/__init__.py
@@ -1,1 +1,5 @@
 from . import utils, config, session, optimizers, conditions, monitors, curriculum, networks, equations, weighting
+
+import logging as _logging
+
+_logging.basicConfig(level=_logging.INFO)
